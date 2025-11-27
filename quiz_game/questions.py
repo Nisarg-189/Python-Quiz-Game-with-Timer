bd526ae7c38cd183e6eceeefa5d72067fea259b0
Nisarg-189 <nis.1.8.9.1.8.9@gmail.com>
2025-11-27 16:03:31 +0530

    Create quiz game with timer

---
+++ quiz_game/questions.py
@@ -0,0 +1,34 @@
+# questions.py
+
+questions = [
+    {
+        "question": "1) What is the capital of Australia?",
+        "options": ["A) Sydney", "B) Canberra", "C) Melbourne", "D) Brisbane"],
+        "answer": "b"
+    },
+    {
+        "question": "2) Who developed Python programming language?",
+        "options": ["A) Guido van Rossum", "B) James Gosling", "C) Dennis Ritchie", "D) Bjarne Stroustrup"],
+        "answer": "a"
+    },
+    {
+        "question": "3) Which planet is known as the Red Planet?",
+        "options": ["A) Venus", "B) Jupiter", "C) Mars", "D) Saturn"],
+        "answer": "c"
+    },
+    {
+        "question": "4) Which data type is immutable in Python?",
+        "options": ["A) List", "B) Set", "C) Dictionary", "D) Tuple"],
+        "answer": "d"
+    },
+    {
+        "question": "5) What does HTML stand for?",
+        "options": [
+            "A) Hyper Trainer Marking Language",
+            "B) Hyper Text Markup Language",
+            "C) Hyper Text Marketing Language",
+            "D) Hyper Technical Markup Level"
+        ],
+        "answer": "b"
+    }
+]
